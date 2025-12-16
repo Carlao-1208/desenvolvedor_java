78b9937928c82a3d388fdf7a7b36f567c17883e4
Carlao-1208 <carlosbob27@gmail.com>
2025-12-15 23:21:40 -0300

    aula do dia 12/12/2025

---
+++ 04_front_end/14_consumo_api/codigo.js/codigo.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+
+
+
+const exibirDados = () => {
+  let nome = document.querySelector('nome').value;
+  let document.querySelector('nome').value;
+  let nome = document.querySelector('nome').value;
+  let nome = document.querySelector('nome').value;
+  let nome = document.querySelector('nome').value;
+  let nome = document.querySelector('nome').value;
+  let nome = document.querySelector('nome').value;
+  let nome = document.querySelector('nome').value;
+}
